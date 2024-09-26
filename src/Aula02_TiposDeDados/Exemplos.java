c26972f7c59248e1a5269fa2c8d8388e34a43c68
Victor Mordachini <mordachini.v@gmail.com>
2024-09-26 08:30:44 -0300

    First Commit

---
+++ src/Aula02_TiposDeDados/Exemplos.java
@@ -0,0 +1,8 @@
+package Aula02_TiposDeDados;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exemplos {
+    public static void main(String[] args) {
+    }
+}

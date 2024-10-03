30b1c7849a5debb354d263f3fbe52eac74091480
Victor Mordachini <mordachini.v@gmail.com>
2024-10-03 08:29:28 -0300

    Aulas Java

--- src/Aula02_TiposDeDados/Exemplos/Exemplos.java
+++ src/Aula02_TiposDeDados/Exemplos/Exemplos.java
@@ -1,4 +1,4 @@
-package Aula02_TiposDeDados;
+package Aula02_TiposDeDados.Exemplos;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.

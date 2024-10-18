e2df64f8f428c83faa5fd03689e05b131350a809
Victor Mordachini <mordachini.v@gmail.com>
2024-10-18 09:20:18 -0300

    18/10/2024

--- src/Aula04_EstruturaDeDecisao/Exercícios/Exercício04.java
+++ src/Aula04_EstruturaDeDecisao/Exercícios/Exercício04.java
@@ -1,4 +1,25 @@
 package Aula04_EstruturaDeDecisao.Exercícios;
 
+import java.util.Scanner;
+
 public class Exercício04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double vendas, comissao;
+        System.out.print("Digite o valor das vendas(R$): ");
+        vendas = scanner.nextDouble();
+
+        if (vendas < 1000) {
+            comissao = 0.05 * vendas;
+            System.out.println("Sua comissão é: R$" + comissao);
+        } else if (vendas >= 1000 && vendas <= 5000) {
+            comissao = 0.1 * vendas;
+            System.out.println("Sua comissão é: R$" + comissao);
+        } else if (vendas > 5000) {
+            comissao = 0.15 * vendas;
+            System.out.println("Sua comissão é: R$" + comissao);
+        }
+
+        scanner.close();
+    }
 }

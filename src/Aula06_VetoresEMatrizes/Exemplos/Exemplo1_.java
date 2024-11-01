16be51383fc2d4097d123176c678bf9ab29ba9ea
Victor Mordachini <mordachini.v@gmail.com>
2024-11-01 08:29:03 -0300

    01/11/2024

---
+++ src/Aula06_VetoresEMatrizes/Exemplos/Exemplo1_.java
@@ -0,0 +1,14 @@
+package Aula06_VetoresEMatrizes.Exemplos;
+
+public class Exemplo1_ {
+    public static void main(String[] args) {
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < numeros.length; i++) {
+
+            System.out.println("Elemento na posição " + i + ": " + numeros[i]);
+
+        }
+
+    }
+}
---
+++ src/Aula06_VetoresEMatrizes/Exercícios/Exercício04.java
@@ -0,0 +1,4 @@
+package Aula06_VetoresEMatrizes.Exercícios;
+
+public class Exercício04 {
+}

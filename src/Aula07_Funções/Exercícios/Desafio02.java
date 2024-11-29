91d31529f009b642efe2b024d7df0b50fb843a4c
Victor Mordachini <mordachini.v@gmail.com>
2024-11-29 09:27:53 -0300

    29/11 - Desafios

--- src/Aula07_Funções/Exercícios/Desafio02.java
+++ src/Aula07_Funções/Exercícios/Desafio02.java
@@ -16,47 +16,55 @@ public class Desafio02 {
                 |    5 - Sair                              |
                 |__________________________________________|
                 """;
+        int opcao;
+        do {
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println(menu);
-        int opcao = scanner.nextInt();
-        scanner.nextLine();
+            switch (opcao) {
+                case 1:
+                    exibirCadastro();
+                    break;
+                case 2:
+                    cadastrarUsuario();
+                    break;
+                case 3:
+                    atualizarUsuario();
+                    break;
+                case 4:
+                    deletarUsuario();
+                    break;
+                case 5:
+                    System.out.println("Fim de programa!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
 
-        switch (opcao) {
-            case 1:
-                exibirCadastro();
-                break;
-            case 2:
-                cadastrarUsuario();
-                break;
-            case 3:
-                atualizarUsuario();
-                break;
-            case 4:
-                deletarUsuario();
-                break;
-            case 5:
-                break;
-            default:
-                System.out.println("Opção inválida!");
+        } while (opcao != 5);
 
-        }
 
         scanner.close();
     }
 
     static void exibirCadastro() {
+        System.out.println("Exibir");
 
     }
 
     static void cadastrarUsuario() {
+        System.out.println("Cadastrar");
 
     }
 
     static void atualizarUsuario() {
+        System.out.println("Atualizar");
 
     }
 
     static void deletarUsuario() {
+        System.out.println("Deletar");
 
     }
 
